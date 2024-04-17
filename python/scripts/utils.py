28e5dbdcb15b2d584ba9fb5ab7e396dcbe5b47da
Cristiane <cristiane.fleal@gmail.com>
2024-04-17 12:06:29 -0300

    update files

--- python/scripts/utils.py
+++ python/scripts/utils.py
@@ -1,32 +1,48 @@
-from datetime import date
+"""Funções de tratamento dos dados"""
+from datetime import datetime
 import os
 import re
 import pandas as pd
-from datetime import datetime
-import mysql.connector
-from sqlalchemy import create_engine   
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
+import mysql.connector
 import logging
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
 class Saneamento:
-    
+    """
+    Class Saneamento
+    Outputs: Realiza o tratamento e limpeza dos dados
+    """
+
     def __init__(self, data, configs):
+        """
+        Função init
+        Outputs: configs
+        """
         self.data = data
         self.metadado =  pd.read_excel(configs["meta_path"])
         self.len_cols = max(list(self.metadado["id"]))
         self.colunas = list(self.metadado['nome_original'])
         self.colunas_new = list(self.metadado['nome'])
-        self.path_work = configs["work_path"]        
+        self.path_work = configs["work_path"]
 
     def select_rename(self):
-        self.data = self.data.loc[:, self.colunas] 
+        """
+        Função select_rename
+        Outputs: Renomeia colunas
+        """
+        self.data = self.data.loc[:, self.colunas]
         for i in range(self.len_cols):
             self.data.rename(columns={self.colunas[i]:self.colunas_new[i]}, inplace = True)
 
     def tipagem(self):
+        """
+        Função tipagem
+        Outputs: Faz a tipagem das colunas
+        """
         for col in self.colunas_new:
             tipo = self.metadado.loc[self.metadado['nome'] == col]['tipo'].item()
             if tipo == "int":
@@ -49,8 +65,12 @@ class Saneamento:
         except AttributeError as e:
             print(f"Erro de atributo: {e}")
             error_handler(e, "AttributeErrorCleanString")
-    
+
     def save_work(self):
+        """
+        Função save_work
+        Outputs: Salva as colunas e os dados já formatados para o sql
+        """
         self.data['load_date'] = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
         user1 = os.getenv('MYSQL_USER')
@@ -59,7 +79,7 @@ class Saneamento:
         table1 = os.getenv('TABLE')
         con = mysql.connector.connect(
             user=user1, password=password1, host='mysql', port="3306", database=database1)
-        
+
         print("DB connected")
 
         engine  = create_engine(f"mysql+mysqlconnector://{user1}:{password1}@mysql/{database1}")
@@ -68,10 +88,14 @@ class Saneamento:
 
 
 def error_handler(exception_error, stage):
-    
+    """
+    Função error_handler
+    Outputs: Trata erro
+    """
+
     log = [stage, type(exception_error).__name__, exception_error,datetime.now()]
     logdf = pd.DataFrame(log).T
-    
+
     if not os.path.exists("logs_file.txt"):
         logdf.columns = ['stage', 'type', 'error', 'datetime']
         logdf.to_csv("logs_file.txt", index=False,sep = ";")
